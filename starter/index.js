6960edef4b602c96fd9d8babb9cc3870adcd5ab9
Caleb I <calebibejigba@gmail.com>
2023-01-31 17:55:34 +0000

    broke down functions into other functions

--- starter/index.js
+++ starter/index.js
@@ -12,6 +12,7 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// arrays of questions for mangers, employees, and Interns
 const mangerQuestions = [
   {
     type: "input",
@@ -99,66 +100,67 @@ const options = [
   },
 ];
 
+
+// function asking the first set of questions to the manger when they first open the application 
 askMore();
 
 async function askMore() {
   await inquirer.prompt(mangerQuestions).then((answers) => {
-    const manager1 = new Manager(
+    const managerOne = new Manager(
       answers.name,
       answers.Id,
       answers.Email,
       answers.Number
     );
-    console.log(manager1);
+    console.log(managerOne);
   });
 
-  repeat()
+  repeat();
 }
 
+// function to repeat questions when user picks option to either employee or intern
 async function repeat() {
-    repeatQuestion()
-  await inquirer.prompt(options);
- 
+  repeatQuestion();
 }
 
 
-async function repeatQuestion(){
-
-    await inquirer.prompt(options).then((answers) => {
-        if (answers.type === "Add an Engineer") {
-          inquirer
-            .prompt(employeeQuestions)
-            .then((answers) => {
-              const engineer1 = new Engineer(
-                answers.name,
-                answers.Id,
-                answers.Email,
-                answers.Number,
-                answers.github
-              );
-              console.log(`picked add an engineer named ${engineer1.name}`);
-            }).then(repeat)
-            
-        }
-         else if (answers.type === "Add an Intern") {
-          inquirer
-            .prompt(InternQuestions)
-            .then((answers) => {
-              const intern1 = new Intern(
-                answers.name,
-                answers.Id,
-                answers.Email,
-                answers.Number,
-                answers.school
-              );
-              console.log(`picked add an intern named ${intern1.id} `);
-            }).then(repeat)
-            
-        } else if (answers.type === "Finish building the team") {
-          console.log("she wants the end");
-        } else {
-          console.log("something is wrong");
-        }
-      });
-
+// functions that display option for employee or intern
+async function repeatQuestion() {
+  await inquirer.prompt(options).then((answers) => {
+    if (answers.type === "Add an Engineer") {
+      inquirer
+        .prompt(employeeQuestions)
+        .then((answers) => {
+          const engineerOne = new Engineer(
+            answers.name,
+            answers.Id,
+            answers.Email,
+            answers.Number,
+            answers.github
+          );
+          console.log(`picked add an engineer named ${engineerOne.name}`);
+        })
+        .then(repeat);
+    } else if (answers.type === "Add an Intern") {
+      inquirer
+        .prompt(InternQuestions)
+        .then((answers) => {
+          const internOne = new Intern(
+            answers.name,
+            answers.Id,
+            answers.Email,
+            answers.Number,
+            answers.school
+          );
+          console.log(`picked add an intern named ${internOne.id} `);
+        })
+        .then(repeat);
+    } else if (answers.type === "Finish building the team") {
+      console.log("she wants the end");
+    } else {
+      console.log("something is wrong");
+    }
+  });
 }
+
+//Manager.getOfficeNumber()
