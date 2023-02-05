a8609dc1a6ae795ab5e3b7e33b7a66190fc256c4
Caleb I <calebibejigba@gmail.com>
2023-02-05 14:52:51 +0000

    improved ui for generated html

--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -100,7 +100,7 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading text-white bg-primary">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
